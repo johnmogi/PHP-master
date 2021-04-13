0cd76cca44d1886ee14f4bf53df72ff31a652e9c
johnmogi <anguru@gmail.com>
2021-04-13 20:18:26 +0300

    exc2

--- jb_lesson_arrays/2020-05-13 - PHP/01 - PHP Basics/excercise2.php
+++ jb_lesson_arrays/2020-05-13 - PHP/01 - PHP Basics/excercise2.php
@@ -2,12 +2,23 @@
 
 $num1 = rand(1,300);
 $num2 = rand(1,300);
-$num3 = 0;
 
-($num2 > $num1 ? $num1 = $num3)
+if( $num2 < $num1) {
+    $num3 = $num2;
+    $num2 = $num1;
+    $num1 = $num3;
+}
 
+for($i = $num1; $i <= $num2; $i++) {
+    echo $i . ", " ;
+}
+echo "<br>";
 
-// echo $num1 . ' ' . $num2;
+// while ($num1 < $num2){
+    
+// }
+
+ echo $num1 . ' ' . $num2;
 
 // echo ($num2 < $num1 ? $num2 . ' ' . $num1 : $num1 . ' ' . $num2);
 
