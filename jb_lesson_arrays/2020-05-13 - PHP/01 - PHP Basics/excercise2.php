1a16cb3c44cde30cfbaf902f02236824209cd8f6
johnmogi <anguru@gmail.com>
2021-04-13 19:56:44 +0300

    practice_basics

---
+++ jb_lesson_arrays/2020-05-13 - PHP/01 - PHP Basics/excercise2.php
@@ -0,0 +1,17 @@
+<?php
+
+$num1 = rand(1,300);
+$num2 = rand(1,300);
+$num3 = 0;
+
+($num2 > $num1 ? $num1 = $num3)
+
+
+// echo $num1 . ' ' . $num2;
+
+// echo ($num2 < $num1 ? $num2 . ' ' . $num1 : $num1 . ' ' . $num2);
+
+
+
+
+?>
